146741c4321aa25aed921034d7b7bf37d631b4f2
PaulCancelEtu <120392278+PaulCancelEtu@users.noreply.github.com>
2023-12-09 13:16:20 +0100

    Add files via upload

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"model.maze"},{"l":"model.maze.observer"},{"l":"model.maze.roles"},{"l":"model.maze.util"}];updateSearchResults();
